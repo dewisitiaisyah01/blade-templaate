eb08a6dddcd7671edc690bdbc2a7e1ccf5aba5ac
dewisitiaisyah01 <dsiti9612@gmail.com>
2024-05-15 10:37:22 +0700

    template-baru

---
+++ resources/views/separate/sidebar.blade.php
@@ -0,0 +1,62 @@
+<div class="sidebar-wrapper">
+        <div class="logo">
+          <a href="javascript:void(0)" class="simple-text logo-mini">
+            CT
+          </a>
+          <a href="javascript:void(0)" class="simple-text logo-normal">
+            Creative Tim
+          </a>
+        </div>
+        <ul class="nav">
+          <li class="active ">
+            <a href="dasboard">
+              <i class="tim-icons icon-chart-pie-36"></i>
+              <p>Dasboard</p>
+            </a>
+          </li>
+          <li>
+            <a href="icon">
+              <i class="tim-icons icon-atom"></i>
+              <p>Icon</p>
+            </a>
+          </li>
+          <li>
+            <a href="notification">
+              <i class="tim-icons icon-bell-55"></i>
+              <p>Notification</p>
+            </a>
+          </li>
+          <li>
+            <a href="user">
+              <i class="tim-icons icon-single-02"></i>
+              <p>User Profile</p>
+            </a>
+          </li>
+          <li>
+            <a href="table">
+              <i class="tim-icons icon-puzzle-10"></i>
+              <p>Table List</p>
+            </a>
+          </li>
+          <li>
+            <a href="./typography.html">
+              <i class="tim-icons icon-align-center"></i>
+              <p>Typography</p>
+            </a>
+          </li>
+          <li>
+            <a href="./rtl.html">
+              <i class="tim-icons icon-world"></i>
+              <p>RTL Support</p>
+            </a>
+          </li>
+          <li class="active-pro">
+            <a href="./upgrade.html">
+              <i class="tim-icons icon-spaceship"></i>
+              <p>Upgrade to PRO</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="main-panel">
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,23 @@ Route::get('form', function () {
 Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('dasboard', function () {
+    return view('template.dasboard');
+});
+
+Route::get('icon', function () {
+    return view('template.icon');
+});
+
+Route::get('notification', function () {
+    return view('template.notification');
+});
+
+Route::get('user', function () {
+    return view('template.user');
+});
+
+Route::get('table', function () {
+    return view('template.table');
+});
